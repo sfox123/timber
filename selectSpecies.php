5e340b7ca47852762b110808442c16b6bd93dc76
SHAGAR Thangaraj <thangaraj.shagar@wfp.org>
2023-03-27 09:47:32 +0530

    species added

---
+++ selectSpecies.php
@@ -0,0 +1,194 @@
+<?php include ('header.php');?>
+<?php include ('top_menu.php');?>
+
+
+</header>
+<style>
+<!--table
+	{mso-displayed-decimal-separator:"\.";
+	mso-displayed-thousand-separator:"\,";}
+@page
+	{margin:.75in .7in .75in .7in;
+	mso-header-margin:.3in;
+	mso-footer-margin:.3in;}
+-->
+</style>
+<div class="container" style="margin-top:18rem">
+<table class="table" border=0 cellpadding=0 cellspacing=0 style='border-collapse:
+ collapse;table-layout:fixed;width:900pt'>
+ <col width=64 span=3 style='width:48pt'>
+ <tr height=25 style='height:18.5pt'>
+  <td height=25 class=xl73 width=64 style='height:18.5pt;width:48pt'>Type of
+  Usage</td>
+  <td class=xl73 width=64 style='border-left:none;width:48pt;box-sizing: border-box'>Conventional
+  Timber</td>
+  <td class=xl73 width=64 style='border-left:none;width:48pt;box-sizing: border-box'>Substitute
+  Timber</td>
+ </tr>
+ <tr height=49 style='height:36.5pt;box-sizing: border-box'>
+  <td height=49 class=xl65 width=64 style='height:36.5pt;width:48pt;box-sizing: border-box'>Beams,
+  Rafters, Ridge Plates, Purling, Wall Plates</td>
+  <td class=xl66 width=64 style='border-left:none;width:48pt;box-sizing: border-box'>Jak,Palu,Eta
+  Thimbiri,Dun</td>
+  <td class=xl66 width=64 style='border-left:none;width:48pt;box-sizing: border-box'>Grandis</td>
+ </tr>
+ <tr height=25 style='height:18.5pt;box-sizing: border-box'>
+  <td height=25 class=xl67 width=64 style='font-size:16pt;height:18.5pt;width:48pt;box-sizing: border-box'>Reepers
+  (2&quot; X 2&quot; &amp; 2&quot; X 1&quot;)</td>
+  <td class=xl68 width=64 style='border-left:none;width:48pt;box-sizing: border-box'>Jak,Wal
+  Del, Gini Sapu, Alastonia</td>
+  <td class=xl68 width=64 style='border-left:none;width:48pt;box-sizing: border-box'>Grandis</td>
+ </tr>
+ <tr height=49 style='height:36.5pt;box-sizing: border-box'>
+  <td height=49 class=xl65 width=64 style='height:36.5pt;width:48pt;box-sizing: border-box'>Valence
+  Board</td>
+  <td class=xl66 width=64 style='border-left:none;width:48pt;box-sizing: border-box'>Wewarana,
+  Panakka, WalDel, Jak, Gini Sapu</td>
+  <td class=xl66 width=64 style='border-left:none;width:48pt;box-sizing: border-box'>Grandis</td>
+ </tr>
+ <tr height=37 style='height:27.5pt;box-sizing: border-box'>
+  <td height=37 class=xl69 width=64 style='height:27.5pt;width:48pt;box-sizing: border-box'>Ceiling
+  Board/Wall Panelling</td>
+  <td class=xl68 width=64 style='border-left:none;width:48pt;box-sizing: border-box'>Teak,
+  Panakka, Lunumidella, Albesia</td>
+  <td class=xl68 width=64 style='border-left:none;width:48pt;box-sizing: border-box'>Cyprus,
+  Boron treated Pinus, Grandis, Albesia</td>
+ </tr>
+ <tr height=37 style='height:27.5pt;box-sizing: border-box'>
+  <td height=37 class=xl65 width=64 style='height:27.5pt;width:48pt;box-sizing: border-box'>Door/Window
+  Frames</td>
+  <td class=xl66 width=64 style='border-left:none;width:48pt;box-sizing: border-box'>Jak,
+  Teak, Milla, Keta Kela, Path Kela, Koon</td>
+  <td class=xl66 width=64 style='border-left:none;width:48pt;box-sizing: border-box'>Microcoris
+  Grandis</td>
+ </tr>
+ <tr height=49 style='height:36.5pt;box-sizing: border-box'>
+  <td height=49 class=xl69 width=64 style='height:36.5pt;width:48pt;box-sizing: border-box'>Skirting</td>
+  <td class=xl68 width=64 style='border-left:none;width:48pt;box-sizing: border-box'>Kolon,
+  Teak, Mahogany, Panakka, Coconut</td>
+  <td class=xl68 width=64 style='border-left:none;width:48pt;box-sizing: border-box'>Grandis</td>
+ </tr>
+ <tr height=49 style='height:36.5pt;box-sizing: border-box'>
+  <td height=49 class=xl70 width=64 style='height:36.5pt;width:48pt;box-sizing: border-box'>Beeralu</td>
+  <td class=xl66 width=64 style='border-left:none;width:48pt;box-sizing: border-box'>Kolon,
+  Mahogany, Satin, Sooriyamara</td>
+  <td class=xl66 width=64 style='border-left:none;width:48pt;box-sizing: border-box'>Grandis</td>
+ </tr>
+ <tr height=36 style='mso-height-source:userset;height:27.0pt;box-sizing: border-box'
+  background="wood back.jpg">
+  <td colspan=3 height=36 class=xl74 width=192 style='border-right:.5pt solid #CCCCCC;
+  height:27.0pt;width:144pt;box-sizing: border-box'>
+  <div style='box-sizing: border-box'>In addition to the above,Timber for
+  parquets, Stairway steps &amp; Hand railings areavailable @the STC depots.</div>
+  </td>
+ </tr>
+ <tr height=60 style='height:45.0pt;box-sizing: border-box'>
+  <td rowspan=2 height=108 class=xl77 width=64 style='border-bottom:.5pt solid #CCCCCC;
+  height:81.0pt;border-top:none;width:48pt;box-sizing: border-box'>Timber for
+  Furniture</td>
+  <td class=xl71 width=64 style='border-top:none;border-left:none;width:48pt;
+  box-sizing: border-box'>Ebony,Klumadiriya,Satin,Nadun, and Teak,Suriyamara ,</td>
+  <td rowspan=2 class=xl71 width=64 style='border-bottom:.5pt solid #CCCCCC;
+  border-top:none;width:48pt;box-sizing: border-box'>Grandis,Cyprus, and
+  Treated Pinus</td>
+ </tr>
+ <tr height=48 style='height:36.0pt'>
+  <td height=48 class=xl72 width=64 style='height:36.0pt;border-left:none;
+  width:48pt'>Hulanghik, Jak, Kohomba, Kolonand variety of Sapu</td>
+ </tr>
+ <![if supportMisalignedColumns]>
+ <tr height=0 style='display:none'>
+  <td width=64 style='width:48pt'></td>
+  <td width=64 style='width:48pt'></td>
+  <td width=64 style='width:48pt'></td>
+ </tr>
+ <![endif]>
+</table>
+
+</div>
+
+
+
+<?php include ('contact-small.php');?>
+
+<?php include ('footer.php');?>
+
+
+
+
+
+<!--===== REQUIRED JS =====--> 
+<script src="js/jquery-3.2.1.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/bootsnav.js"></script>
+
+<!--To View on scroll-->
+<script src="js/jquery.appear.js"></script>
+ 
+<!--Owl Slider-->
+<script src="js/owl.carousel.min.js"></script>
+
+<!--Parallax-->
+<script src="js/parallaxie.js"></script>
+
+<!--Fancybox-->
+<script src="js/jquery.fancybox.min.js"></script> 
+
+<!--Cube Gallery-->
+<script src="js/cubeportfolio.min.js"></script> 
+
+<!--Bootstrap Dropdown-->
+<script src="js/bootstrap-select.js"></script>
+
+<!--Video Popup-->
+<script src="js/videobox/video.js"></script>
+
+<!--Datepicker-->
+<script src="js/datepicker.js"></script> 
+
+<!--Dropzone-->
+<script src="js/dropzone.min.js"></script>
+
+<!--Wow animation-->
+<script src="js/wow.min.js"></script>
+
+<!--Rang Slider-->
+<script src="js/range-Slider.min.js"></script> 
+
+<!--Checkbox-->
+<script src="js/selectbox-0.2.min.js"></script> 
+
+<!--Checkbox-->
+<script src="js/scrollreveal.min.js"></script> 
+
+<!--Checkbox-->
+<script src="js/jquery-countTo.js"></script> 
+
+<!--Checkbox-->
+<script src="js/jquery.typewriter.js"></script> 
+
+<!--Checkbox-->
+<script src="js/death.min.js"></script>
+
+<!--Revolution Slider-->
+<script src="js/themepunch/jquery.themepunch.tools.min.js"></script>
+<script src="js/themepunch/jquery.themepunch.revolution.min.js"></script>   
+<script src="js/themepunch/revolution.extension.layeranimation.min.js"></script> 
+<script src="js/themepunch/revolution.extension.navigation.min.js"></script> 
+<script src="js/themepunch/revolution.extension.parallax.min.js"></script> 
+<script src="js/themepunch/revolution.extension.slideanims.min.js"></script> 
+<script src="js/themepunch/revolution.extension.video.min.js"></script>
+
+<!--Custom Js -->
+<script src="js/functions.js"></script>
+
+<!--Maps & Markers-->
+<script src="js/form.js"></script> 
+<script src="js/custom-map.js"></script> 
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAOBKD6V47-g_3opmidcmFapb3kSNAR70U"></script>
+<script src="js/gmaps.js"></script>
+<script src="js/contact.js"></script> 
+<!--===== #/REQUIRED JS =====-->
+
+</body>
+</html>
